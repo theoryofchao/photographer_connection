7a5d58e48a20ab2f8f56105ffc9282711e7740d8
theoryofchao <roger.ti.chao@gmail.com>
2017-02-22 14:31:29 -0500

    added npm installs for project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieSession());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -44,3 +44,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
