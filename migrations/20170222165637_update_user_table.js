8d0b693502b33881ccb602a238598948ac982ac2
theoryofchao <roger.ti.chao@gmail.com>
2017-02-22 18:30:59 -0500

    modified users table and improved package json

---
+++ migrations/20170222165637_update_user_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function (table) {
+      table.integer('status').notNullable().comment('-1: banned, 0: inactive, 1: active, 2: deactivated, 3: deleted');
+    })
+  ]) 
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    table.dropColumn('status')
+  ]) 
+};
