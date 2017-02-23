e2010eb9222885aa35baf92628322d31c0d3ae96
deefourple <davedehghani@gmail.com>
2017-02-23 19:47:06 +0000

    Implemented bootstrap-react NPM package

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './Test.js';
+import { Button } from 'react-bootstrap';
+
+const buttonsInstance = (
+  <Button bsSize="large">Click me!</Button>
+);
 
 class App extends Component {
   render() {
@@ -11,9 +16,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro blockquote-reverse">
           To get started, edit <code>src/App.js</code> and save to reload.
         <Test />
+        {buttonsInstance}
         </p>
 
       </div>
