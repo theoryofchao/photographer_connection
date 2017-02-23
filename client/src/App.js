edbd0ba8c2f005a0b887fd8358b3655889ce2e4f
deefourple <davedehghani@gmail.com>
2017-02-23 18:09:16 +0000

    Rendered a component to the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test.js';
 
 class App extends Component {
   render() {
@@ -12,7 +13,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        <Test />
         </p>
+
       </div>
     );
   }
---
+++ client/src/Test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+
+class Test extends Component {
+  render() {
+    return (
+        <div>
+          <h5> TEST TEST TEST </h5>
+        </div>
+    );
+  }
+}
+
+export default Test;
