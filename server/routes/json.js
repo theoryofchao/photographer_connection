34d0f713f61faadea7e92b1390a49ef6e04a681f
theoryofchao <roger.ti.chao@gmail.com>
2017-02-23 13:17:03 -0500

    starting routing

--- server/routes/json.js
+++ server/routes/json.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let getKnex = (req) => {
+  return req.app.get('knex');
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,7 +12,25 @@ router.get('/', function(req, res, next) {
 
 /* REGISTER user account */
 router.post('/register', (req, res, next) => {
-  res.send('blah');
+  const knex = getKnex(req);
+  let currentTime = new Date();
+  currentTime = Date.now();
+  let result = knex(`users`).insert({ email: "roger.ti.chao@gmail.com",
+                                      password: "password",
+                                      created_at: currentTime
+  });
+  console.log(result);
+  res.send('great success');
+});
+
+/* LOGIN user account */
+router.post('/login', (req, rest, next) => {
+  //login logic
+});
+
+/* LOGOUT user account */
+router.delete('/logout', (req, rest, next) => {
+  //logout logic
 });
 
 module.exports = router;
