9e3dfaa6f85fad23891ce8bb0b80310acb20462f
deefourple <davedehghani@gmail.com>
2017-02-28 14:28:59 -0500

    Added background image and navbar

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-// import './App.css';
-import Nav from './Nav.js'
-import Search from './Search.js'
+import Navigate from './Nav.js'
 
 var borderStyles = {
-  border: "solid 1px black",
-  padding: "10px"
+  borderBottom: "solid 2px black",
 }
 
 class Header extends Component {
   render() {
-
     return (
-        <div style={borderStyles}>
-          Header
-            <Nav userA={this.props.userA} onLogoutClick={this.props.onLogoutClick}/>
-            <br />
-            <Search />
-        </div>
+      <div style={borderStyles}>
+          <Navigate userA={this.props.userA} onLogoutClick={this.props.onLogoutClick}/>
+      </div>
     );
   }
 }
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
+import {  NavItem, Nav } from 'react-bootstrap';
 
-var borderStyles = {
-  border: "solid 1px black",
-  padding: "10px"
+var tagline = {
+  color: "white",
+  textAlign: "left",
+  paddingLeft: "970px",
+  backgroundColor: "black",
+  background: "transparent"
 }
 
-class Nav extends Component {
+var styles = {
+  backgroundImage: "url(http://www.newhdwallpapers.in/wp-content/uploads/2013/09/Holidays-Wide-Photography.jpg)",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  color: "white",
+  textDecoration: "none",
+  lineHeight: "3em",
+  fontSize: "1.5em",
+  textAlign: "right",
+  height: "800px",
+  opacity: "0.7"
+}
+
+class Navigate extends Component {
   render() {
-      if (this.props.userA) {
-        return (
-          <div style={borderStyles}>
-            Nav
-            <br />
-            <Link to="/">Home</Link><br />
-            <Link to="/my-profile">My Profile</Link><br />
-            <Link to='/' onClick={(e)=>{this.props.onLogoutClick()}}>Logout</Link><br />
-            {this.props.children}
-          </div>
-          )
-      } else {
-    return (
-        <div style={borderStyles}>
-          Nav
-          <br />
-          <Link to="/">Home</Link><br />
-          <Link to="/register">Register</Link> <br />
-          <Link to="/login">Login</Link><br />
-          {this.props.children}
+    if (this.props.userA) {
+      return (
+        <div style={styles}>
+          <Nav bsStyle="pills">
+            <NavItem bsStyle="pills" href="/#/">Home</NavItem>
+            <NavItem bsStyle="pills" href="/#/my-profile">My Profile</NavItem>
+            <NavItem bsStyle="pills" href="/#/" onClick={(e)=>{this.props.onLogoutClick()}}>Logout</NavItem>
+              <div style={tagline}>
+                <h3>Find a local photographer</h3>
+              </div>
+          </Nav>
+        </div>
+       )
+    } else {
+      return (
+        <div style={styles}>
+           <Nav bsStyle="pills">
+              <NavItem href="/#/">Home</NavItem>
+              <NavItem href="/#/register">Register</NavItem>
+              <NavItem href="/#/login">Login</NavItem>
+                <div style={tagline}>
+                  <h3>Find a local photographer</h3>
+                </div>
+           </Nav>
         </div>
     )}
   }
 }
 
-export default Nav;
+export default Navigate;
