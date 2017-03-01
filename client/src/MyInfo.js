c7f4af2509eeccefcc91de10c916875af7837903
ChrisBotCodes <chrisadambot@gmail.com>
2017-03-01 18:09:56 -0500

    fixed spacing issue

--- client/src/MyInfo.js
+++ client/src/MyInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import TextField from 'material-ui/TextField'
+
 var parent = {
   display: "inline-block",
   padding: "25px",
@@ -8,24 +9,30 @@ var parent = {
   width: "90%",
   backgroundImage: "radial-gradient(white, black)"
 }
+
 var titleStyle = {
   display: "inline-block",
   color: "black",
   padding: "35px"
 }
+
   var child = {
     padding: "20px"
   }
+
 class MyInfo extends Component {
+
   onImageDrop(files) {
     this.props.handleProfileImageUpload(files[0]);
     this.props.handleImageUpload(files[0], 'http://localhost:8080/users/profile_image');
   }
+
   onFormSubmit = (e) => {
     e.preventDefault();
     console.log('CURRENT USER:', this.props.myProfile);
     this.props.onInfoSubmit(this.props.myProfile);
   }
+
   render() {
     return (
       <div style={parent}>
@@ -38,6 +45,7 @@ class MyInfo extends Component {
             <p>Drop an image or click to select a file to upload</p>
           </Dropzone>
         </div>
+
         <div>
           {this.props.uploadedFileCloudinaryUrl === '' ? null :
           <div>
@@ -45,6 +53,7 @@ class MyInfo extends Component {
             <img src={this.props.uploadedFileCloudinaryUrl} role="presentation" />
           </div>}
         </div>
+
         <div className="form">
           <form style={child} onSubmit={this.onFormSubmit.bind(this)} >
             <TextField
