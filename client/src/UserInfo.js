6b88ff7da472660f0c41a825c1b6b13525fb6b28
deefourple <davedehghani@gmail.com>
2017-03-01 16:09:35 -0500

    Styled basic outline of user page

--- client/src/UserInfo.js
+++ client/src/UserInfo.js
@@ -1,29 +1,57 @@
 import React, { Component } from 'react';
 // import './App.css';
 
+var style = {
+  backgroundColor: "lightgrey",
+  opacity: "0.7",
+  height: "200px"
+}
+
 var profile = {
   display: "inline-block",
-  width: "150px",
   height: "150px",
   borderRadius: "50%",
-  backgroundRepeat: "no-repeat",
-  backgroundPosition: "center center",
-  backgroundSize: "cover"
-
+  float: "left",
+  padding: "25px"
 }
+
+var info = {
+  display: "inline-block",
+  padding: "25px 180px 25px 50px",
+  margin: "0 auto",
+  listStyleType: "none"
+}
+
+var listItems = {
+  padding: "7px 0"
+}
+
+var descript = {
+  display: "inline-block",
+  verticalAlign: "top",
+  padding: "7px 0 7px 30px"
+}
+
 
 class UserInfo extends Component {
   render() {
     return (
-        <div>
-          <img style={profile} src="https://img.clipartfest.com/ae3134c8983b10e4b65d9777294cec41_profile-icon-clip-art-profile-icon-clipart_300-300.png" role="presentation" />
-          <h4>handle</h4>
-          <h4>firstName lastName</h4>
-          <h5>location</h5>
-          <h5>YOE</h5>
-          <p>description</p>
-          <h5>Contact: {this.props.userProfile.email}</h5>
-          <br />
+        <div style={style}>
+          <div>
+            <img style={profile} src="https://img.clipartfest.com/ae3134c8983b10e4b65d9777294cec41_profile-icon-clip-art-profile-icon-clipart_300-300.png" role="presentation" />
+            </div>
+            <div>
+              <ul style={info}>
+                <li style={listItems}>Handle</li>
+                <li style={listItems}>FirstName LastName</li>
+                <li style={listItems}>Location:</li>
+                <li style={listItems}>Years Of Experience:</li>
+                <li style={listItems}>Contact: {this.props.userProfile.email}</li>
+              </ul>
+              <p style={descript}>Description</p>
+            </div>
+
+
         </div>
     );
   }
