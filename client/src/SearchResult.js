6158b9cfc5367a328f51fe269f4a4be1f3186c0f
ChrisBotCodes <chrisadambot@gmail.com>
2017-03-01 20:11:44 -0500

    added profile picture, name, email, location to search results

--- client/src/SearchResult.js
+++ client/src/SearchResult.js
@@ -4,8 +4,12 @@ import { Link } from 'react-router';
 
 var styles = {
   border: "solid 1px black",
-  padding: "10px 10px 150px 10px",
+  padding: "10px 10px 15px 10px",
   overflow: "auto"
+}
+
+var img = {
+  height: "115px"
 }
 
 class SearchResult extends Component {
@@ -14,8 +18,9 @@ class SearchResult extends Component {
     let profileLink = "/user-profile/" + this.props.result.user_id;
     return (
         <div style={styles}>
-          <p>First + Last Name </p>
-          <p>location</p>
+          <p>{this.props.result.first_name} {this.props.result.last_name}</p>
+          <img style={img} src={this.props.result.profile_picture} role="presentation"/>
+          <p>{this.props.result.location_string}</p>
           <p>$$$$ Icon?</p>
           <Link to={profileLink}>{this.props.result.email}</Link>
         </div>
